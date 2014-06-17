51a1f9a110f563daf242b2354869278240020669
cesarmb <cesarmatab@gmail.com>
2014-06-17 11:19:45 -0600

    Update OJS_SciELO_DAO.inc.php

--- OJS_SciELO_DAO.inc.php
+++ OJS_SciELO_DAO.inc.php
@@ -1,23 +1,22 @@
 // In progress
 
 <?php
-
+/*
+ * The intention with this class is to encapsulate the necesary data for this plugin in a DAO object. This DAO will contain extra information about
+ * publications. For instance: the body, citations and count of images among othrers.
+ */
 class OJS_SciELO_DAO extends DAO {
 
-	function OJS_SciELO_DAO() {
+	function OJS_SciELO_DAO() { // DAO's constructor.
 		parent::DAO();
 	}
 
-	function getArticleBody($articleId) {
-		$primaryLocale = Locale::getPrimaryLocale();
-		$result = &$this->retrieve('SELECT setting_value FROM article_settings WHERE 
-									setting_name=\'body\' AND 
-									article_id = ? AND 
-									locale = ? ', 
-									array($articleId, $primaryLocale));
-		return $result->fields['setting_value'];
-	}
-
+	/*
+	 * This function checks if a determinate setting (attribute) is already part of the corresponding table.
+	 * @$articleId: article identifier ($article -> getID()).
+	 * @$setting_name: setting to be checked into the database.
+	 * @$locale: localization of the article.
+	 */
 	function settingExists($articleId, $settingName, $locale = null){
 		if($locale == null) $locale = Locale::getPrimaryLocale();
 		$result = &$this->retrieve('SELECT setting_value FROM article_settings WHERE 
@@ -133,7 +132,19 @@ class OJS_SciELO_DAO extends DAO {
 			)
 	 	);
 	}
+
+	function countImagesByArticleId($articleId){
+		$locale = Locale::getPrimaryLocale();
+		$result = &$this->retrieve('SELECT setting_value FROM article_settings WHERE 
+									setting_name = ? AND 
+									article_id = ? AND 
+									locale = ?', 
+									array("images", $articleId, $locale)
+								   );
+		return $result->RecordCount();
+	}
 	// ================= </IMAGES> ================= \\
+
 
 }
 
