59fb45710ac7fa88a375354725615219afc6547b
cesarmb <cesarmatab@gmail.com>
2014-06-26 09:36:00 -0600

    Creación del archivo handler en el repositorio

---
+++ OJS2SciELOHandler.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+import('classes.handler.Handler');
+import('file.ArticleFileManager');
+import('lib.pkp.classes.core.JSONManager');
+import('lib.pkp.classes.core.JSONMessage');
+
+class OJS2SciELOHandler extends Handler {
+
+}
+
+?>
